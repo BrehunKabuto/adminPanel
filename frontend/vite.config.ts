daab5befc19dbfccb154021db26aeab87a282e87
BrehunKabuto <vova1001lp@gmail.com>
2026-07-12 20:49:52 +0300

    add vite plugin svgr

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from 'vite-plugin-svgr'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
